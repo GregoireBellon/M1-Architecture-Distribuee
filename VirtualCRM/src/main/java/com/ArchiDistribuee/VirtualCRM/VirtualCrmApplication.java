149cd119b28b975d5c8819192aa719cf6be2f470
Grégoire Bellon <gregoire.bellon@outlook.fr>
2023-10-13 11:06:50 +0200

    First commit !

---
+++ VirtualCRM/src/main/java/com/ArchiDistribuee/VirtualCRM/VirtualCrmApplication.java
@@ -0,0 +1,13 @@
+package com.ArchiDistribuee.VirtualCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCrmApplication.class, args);
+	}
+
+}
---
+++ VirtualCRM/src/test/java/com/ArchiDistribuee/VirtualCRM/VirtualCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArchiDistribuee.VirtualCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
