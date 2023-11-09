35383f030b2c5ed25a6fcf675ec1add9438ea8a0
Romain Supiot <romain.supiot@dametis.com>
2023-11-09 15:34:37 +0100

    feat(VirtualCRM): init spring, mapstruct, lombok and setup architecture

--- VirtualCRM/build.gradle.kts
+++ VirtualCRM/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.4"
+	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
 	id("io.freefair.lombok") version "8.4"
 
@@ -20,7 +20,10 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation ("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor ("org.mapstruct:mapstruct-processor:1.5.5.Final")
 }
 
 tasks.withType<Test> {
---
+++ VirtualCRM/src/main/java/com/ArchiDistribuee/VirtualCRM/controller/FakeController.java
@@ -0,0 +1,10 @@
+package com.ArchiDistribuee.VirtualCRM.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+public class FakeController {
+
+}
