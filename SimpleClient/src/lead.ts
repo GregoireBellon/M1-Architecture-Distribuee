80d38313aaaaac38405149b644d76d1f74491b66
Romain Supiot <romain.supiot@dametis.com>
2023-11-13 15:25:11 +0100

    feat(SimpleClient): improvements

--- SimpleClient/src/lead.ts
+++ SimpleClient/src/lead.ts
@@ -1,15 +1,15 @@
 export interface Lead {
-    id: string,
-    firstName: string,
-    lastName: string,
-    annualRevenue: number,
-    phone: string,
-    street: string,
-    postalCode: string,
-    city: string,
-    country: string,
-    creationDate: Date,
-    // geographicalPoint : ;
-    company: string,
-    state: string
-};
+  id: string;
+  firstName: string;
+  lastName: string;
+  annualRevenue: number;
+  phone: string;
+  street: string;
+  postalCode: string;
+  city: string;
+  country: string;
+  creationDate: Date;
+  geographicalPoint: { latitude: number; longitude: number };
+  company: string;
+  state: string;
+}
--- VirtualCRM/src/main/java/com/ArchiDistribuee/VirtualCRM/repository/SalesForceCRMRepository.java
+++ VirtualCRM/src/main/java/com/ArchiDistribuee/VirtualCRM/repository/SalesForceCRMRepository.java
@@ -4,7 +4,6 @@ import com.ArchiDistribuee.VirtualCRM.entity.SalesForceLead;
 
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashSet;
@@ -12,11 +11,6 @@ import java.util.Calendar;
 
 @Repository
 public class SalesForceCRMRepository {
-    @Value("${salesforcepassword}")
-    private String password;
-
-    @Value("${salesforcelogin}")
-    private String login;
 
     // TODO
     public Set<SalesForceLead> getLeads(double lowAnnualRevenue, double highAnnualRevenue, String state) {
