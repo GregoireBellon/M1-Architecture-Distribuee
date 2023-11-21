92727b70e5e6dd83ff78fb4c4a0102ea396f7229
Grégoire Bellon <gregoire.bellon@outlook.fr>
2023-11-21 10:36:31 +0100

    chore : paramêtrisation client

--- SimpleClient/src/internal-crm-client.ts
+++ SimpleClient/src/internal-crm-client.ts
@@ -6,8 +6,11 @@ import { Lead } from './lead';
 import { toThriftInternalLeadDto } from './mapper/lead-mapper';
 
 export class InternalCrmClient {
+  private host: string = process.env.THRIFT_HOST ?? 'localhost';
+  private port: number = parseInt(process.env.THRIFT_PORT ?? '9090');
+
   private async connectionWrapper<T>(operation: (client: Readonly<Client>) => T): Promise<T> {
-    const conn = createConnection('localhost', 9090, {
+    const conn = createConnection(this.host, this.port, {
       transport: TBufferedTransport,
       protocol: TBinaryProtocol,
     });
